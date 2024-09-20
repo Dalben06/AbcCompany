082329b2dee391b5714fb2f46f1d6cf475dee740
Leonardo Dalben <leo.dalben@gmail.com>
2024-09-20 10:15:22 -0500

    Add Scripts 1 to add Order Tables and Database

---
+++ Sqls/Script1.sql
@@ -0,0 +1,44 @@
+-- Criação do banco de dados
+CREATE DATABASE AbcCompany;
+GO
+
+USE AbcCompany;
+GO
+
+-- Tabela Order
+CREATE TABLE Orders (
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    OrderNumber INT NOT NULL,
+    ClientId INT NOT NULL,
+    ClientName NVARCHAR(100),
+    BranchId INT,
+    BranchName NVARCHAR(100),
+    Total DECIMAL(18, 2),
+    DiscountTotal DECIMAL(18, 2),
+    StatusOrderId INT,
+    StatusOrderName NVARCHAR(50)
+);
+GO
+
+-- Tabela OrderProducts
+CREATE TABLE OrderProducts (
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    OrderId INT FOREIGN KEY REFERENCES [Orders](Id),
+    ProductId INT NOT NULL,
+    ProductName NVARCHAR(100),
+    ProductUnitValue DECIMAL(18, 2),
+    ProductQuantity INT,
+    Discount DECIMAL(18, 2),
+    Total DECIMAL(18, 2)
+);
+GO
+
+-- Tabela OrderPayments
+CREATE TABLE OrderPayments (
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    OrderId INT FOREIGN KEY REFERENCES [Orders](Id),
+    PaymentId INT NOT NULL,
+    PaymentName NVARCHAR(50),
+    Value DECIMAL(18, 2)
+);
+GO
